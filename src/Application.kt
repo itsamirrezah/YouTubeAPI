a5ffb4ddadaf6181d190b05b6589fa21f990b8c7
Amirreza H <itsamirrezah@gmail.com>
2020-08-27 21:06:21 +0430

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,27 @@
+package com.itsamirrezah.youtubeapi
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.gson.*
+import io.ktor.features.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(ContentNegotiation) {
+        gson {}
+    }
+
+    routing {
+        get("/") {
+            call.respondText("Test Success!", contentType = ContentType.Text.Plain)
+        }
+
+    }
+}
+
