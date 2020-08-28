58bdb5f761b7c0f0ce41406c6fb64264d5f86505
Amirreza H <itsamirrezah@gmail.com>
2020-08-28 20:03:34 +0430

    Build youtube api interface

---
+++ src/YouTubeAPI.kt
@@ -0,0 +1,55 @@
+package com.itsamirrezah.youtubeapi
+
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.ResponseBody
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface YouTubeAPI {
+
+    @GET("/youtube/v3/playlistItems?part=snippet&maxResults=50")
+    fun getPlaylistItems(
+        @Query("playlistId") playlistId: String,
+        @Query("pageToken") pageToken: String  //point to the next page
+    ): Call<ResponseBody>
+}
+
+object RetrofitBuilder {
+
+    var youtube: YouTubeAPI
+    private var httpClient = OkHttpClient.Builder()
+
+    init {
+        loggingInterceptor()
+        //add google api key to all requests
+        httpClient.interceptors().add(object : Interceptor {
+            override fun intercept(chain: Interceptor.Chain): Response {
+                var request = chain.request()
+                val httpUrl = request.url.newBuilder().addQueryParameter("key", "GOOGLE_API_KEY").build()
+                request = request.newBuilder().url(httpUrl).build()
+                return chain.proceed(request)
+            }
+        })
+
+        youtube = Retrofit.Builder()
+            .baseUrl("https://www.googleapis.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(httpClient.build())
+            .build()
+            .create(YouTubeAPI::class.java)
+    }
+
+    private fun loggingInterceptor() {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        httpClient.addInterceptor(logging)
+    }
+}
+
